e478743ee894f0caf5b482bd948d8090b127c998
Ziwen Zhao <844766049@qq.com>
2019-04-10 17:52:09 +0800

    Add files via upload

---
+++ CPR AED/Scripts else/ColorChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ColorChanger : MonoBehaviour {
+    public GazeTriggerTest gazeControl;
+    public Material origin;
+    public Material highlight;
+    // Use this for initialization
+    void Start () {
+        //gazeControl.FocusEnter += HighlightColor;
+        //gazeControl.FocusExit += originalColor;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void HighlightColor ()
+    {
+        GetComponent<Renderer>().material.color = highlight.color;//object change color to highlight 
+    }
+
+    void originalColor ()
+    {
+        GetComponent<Renderer>().material.color = origin.color;//object change color to highlight 
+    }
+}
---
+++ CPR AED/Scripts else/GazeOn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using HoloToolkit.Unity.InputModule;
+
+public class GazeOn : MonoBehaviour, IFocusable
+{
+    public bool ifGazed = false;
+    //public UnityEvent FocusEnterEvent;
+    //public UnityEvent FocusLostEvent;
+    // Use this for initialization
+
+
+    public void OnFocusEnter()
+    {
+        ifGazed = true;
+    }
+
+    public void OnFocusExit()
+    {
+        ifGazed = false;
+    }
+}

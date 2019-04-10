b97ea27a20bdc205f93ce4411c787e0af3ad0749
Ziwen Zhao <844766049@qq.com>
2019-04-10 17:35:26 +0800

    Add files via upload

---
+++ Lefthand.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.UI;
+
+
+public class Lefthand: MonoBehaviour {
+    public Transform leftHand;
+    public Transform rightHand;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        leftHand.localPosition = InputTracking.GetLocalPosition(XRNode.LeftHand);
+        rightHand.localPosition = InputTracking.GetLocalPosition(XRNode.RightHand);
+        Text Text0;
+        GameObject.Find("Text0").GetComponent<Text>().text = leftHand.localPosition.y.ToString();
+    }
+}

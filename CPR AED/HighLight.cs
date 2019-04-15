082dab3bb1caff6015a3866cdb2d6f60ba40dabc
Ziwen Zhao <844766049@qq.com>
2019-04-15 12:18:17 +0800

    Add some scripts

---
+++ CPR AED/HighLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighLight : MonoBehaviour {
+    public GameObject ball;
+    public Material highlight;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider interactObject)
+    {
+        ball.GetComponent<Renderer>().material.color = highlight.color;//object change color to highlight 
+
+        //ifTouched = true;
+    }
+}
---
+++ CPR AED/testEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine.Events;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testEvent : MonoBehaviour {
+
+    public UnityAction action;
+    public UnityEvent myEvent = new UnityEvent();
+
+    void Start()
+    {       
+        action = new UnityAction(MyFunction);
+        action+=MyFunction2;
+        myEvent.AddListener(action);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            myEvent.Invoke();
+        }
+    }
+
+    public void MyFunction()
+    {
+        print ("Hello: ");
+    }
+
+    public void MyFunction2()
+    {
+        print ("Hello2: ");
+    }
+
+}

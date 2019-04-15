3a9be65780d9fe95bfc29ea2fc4ec713ef784cbf
Ziwen Zhao <844766049@qq.com>
2019-04-15 12:15:55 +0800

    Add some scripts

---
+++ CPR AED/GazeTracker.cs
@@ -0,0 +1,37 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using HoloToolkit.Unity.InputModule;
+using VRStandardAssets.Utils;
+
+public class GazeTracker : MonoBehaviour, IFocusable
+{
+    public UnityAction FocusEnter;
+    public UnityAction FocusExit;
+    public bool ifGazed = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnFocusEnter()
+    {
+        if(FocusEnter != null) FocusEnter();
+        gameObject.GetComponent<SelectionSlider>().m_GazeOver = true;
+    }
+
+    public void OnFocusExit()
+    {
+        if (FocusExit != null) FocusExit();
+        gameObject.GetComponent<SelectionSlider>().m_GazeOver = false;
+    }
+}
